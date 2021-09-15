a00466caf78d9ab2a08e543ca98535dd49a20278
Texx45 <tcalderbank@hotmail.com>
2021-09-15 20:30:44 +0100

    finshed greeting exercise

--- exercises/fetch-exercise/exercise.js
+++ exercises/fetch-exercise/exercise.js
@@ -17,10 +17,18 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch("*** Write the API address here ***")
+const renderGreeting = document.querySelector("#greeting-text");
+
+// fetch("https://codeyourfuture.herokuapp.com/api/greetings")
+//   .then((response) => response.text())
+//   .then((greeting) => (renderGreeting.innerText = greeting));
+
+//! Original code
+
+fetch("https://codeyourfuture.herokuapp.com/api/greetings")
   .then(function (response) {
     return response.text();
   })
   .then(function (greeting) {
-    // Write the code to display the greeting text here
+    renderGreeting.innerText = greeting;
   });
